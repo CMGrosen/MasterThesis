51204f2ce1d06eb222ff4e58023414612f34517f
JacobSvenningsen <jacobsvenningsen15@hotmail.com>
2019-11-21 11:07:48 +0100

    Fill out state. Start work on generating successors. A state possibly need to contain more information about where we came from, for example

--- main.cpp
+++ main.cpp
@@ -14,13 +14,43 @@ static std::map< const char *, const char * > files = {
         {"concurrency", "../code_examples/concurrency.small"},
         {"out_of_bounds", "../code_examples/out_of_bounds.small"},
         {"race_condition", "../code_examples/race_condition.small"},
-        {"bubblesort", "../code_examples/bubblesort.small"}
+        {"bubblesort", "../code_examples/bubblesort.small"},
+        {"stateTest", "../nodeptr_test.small"}
 };
+
+//definitely have to think carefully about how we do this
+std::vector<state> f(state* s) {
+    switch(s->get_position()->getNodeType()) {
+        case BinaryExpression:
+            return std::vector<state>();
+        case Literal:
+            return std::vector<state>();
+        case Assign:
+            if(auto a = dynamic_cast<assignNode*>(s->get_position())) {
+                if (auto b = dynamic_cast<binaryExpressionNode*>(a->getExpr())) {
+                    auto table = std::unordered_map<std::string, std::shared_ptr<expressionNode>>{};
+                    std::shared_ptr<expressionNode> expr = DST::deepCopy(b);
+                    if (expr->getNodeType() == BinaryExpression && expr->getType() == intType) {
+                        if(((binaryExpressionNode*)expr.get())->getLeft()->getNodeType() == Literal
+                        && ((binaryExpressionNode*)expr.get())->getRight()->getNodeType() == Literal) {
+                            std::string lVal = ((literalNode*)((binaryExpressionNode*)expr.get())->getLeft())->value;
+                            std::string rVal = ((literalNode*)((binaryExpressionNode*)expr.get())->getRight())->value;
+                            table.insert({a->getName(), std::make_shared<literalNode>(expr->getType(), std::to_string(stoi(lVal) + stoi(rVal)))});
+                        }
+                    }
+                    return std::vector<state>{state(nullptr, table)};
+                }
+            }
+            return std::vector<state>();
+        default:
+            return std::vector<state>();
+    }
+}
 
 int main(int argc, const char* argv[]) {
     std::ifstream stream;
     //stream.open("../code.small");
-    stream.open(files["bubblesort"]);
+    stream.open(files["stateTest"]);
     //stream.open("shortExpr.small");
 
     ANTLRInputStream input(stream);
@@ -40,6 +70,11 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     typeof(treeAndSymbolTable.first) a = DST::deepCopy(treeAndSymbolTable.first.get());
+
+    auto table = std::unordered_map<std::string, std::shared_ptr<expressionNode>>();
+    table.insert({"a", std::make_shared<binaryExpressionNode>(binaryExpressionNode(intType, PLUS, std::make_shared<literalNode>(literalNode(intType, "2")),std::make_shared<literalNode>(literalNode(intType, "2"))))});
+    state no = state(a.get(), std::move(table));
+    std::vector<state> succStates = no.get_successors(f);
     std::cout << "got here" << std::endl;//a << std::endl;
     return 0;
 }
--- symengine/state.hpp
+++ symengine/state.hpp
@@ -4,6 +4,24 @@
 #ifndef ANTLR_CPP_TUTORIAL_STATE_HPP
 #define ANTLR_CPP_TUTORIAL_STATE_HPP
 
+#include <functional>
+#include <iostream>
 #include "symbolTable.hpp"
+class state {
+public:
+    state(node *current_position, std::unordered_map<std::string, std::shared_ptr<expressionNode>> constraints) : cPos{current_position}, table{std::move(constraints)} {};
+    node *get_position() const {return cPos;}
+    std::vector<state> get_successors(std::vector<state> (*func)(state*)) {
+        return (*func)(this);
+    }
+private:
+    node *cPos;
+    /* Still don't see why symbolic and concrete
+     * cannot just be pointers to expression nodes.
+     * Concrete is just a Literal
+     * Symbolic is a BinaryExpression
+     */
+    std::unordered_map<std::string, std::shared_ptr<expressionNode>> table;
+};
 
 #endif //ANTLR_CPP_TUTORIAL_STATE_HPP
