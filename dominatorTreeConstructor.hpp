9326340889cb6b26693acf9da1d6113ae10401ea
CGrosen <cgrose15@student.aau.dk>
2020-02-20 15:54:58 +0100

    DominatorTree maybe works

--- dominatorTreeConstructor.hpp
+++ dominatorTreeConstructor.hpp
@@ -35,7 +35,7 @@ public:
     }
 
     static void CreateDFSTreeHelper(const std::shared_ptr<basicblock>& _node, const std::shared_ptr<basicblock>& _parent, int* num, DFSTree* dfsTree){
-        if( dfsTree->parents.find(_node) == dfsTree->parents.end() ){
+        if( dfsTree->dfnums.find(_node) == dfsTree->dfnums.end() ){
             dfsTree->parents.insert({_node, _parent});
             dfsTree->nodes.insert({*num, _node});
             dfsTree->dfnums.insert({_node, *num});
@@ -55,9 +55,11 @@ public:
 
     static std::shared_ptr<basicblock>* AncestorWithLowestSemi(std::shared_ptr<basicblock>* v, DFSTree* dfsTree, std::unordered_map<std::shared_ptr<basicblock>, std::shared_ptr<basicblock>>* semi){
         auto u = v;
-        while(dfsTree->ancestors.find(*v) != dfsTree->ancestors.end()){
-            int v_dfnum = dfsTree->dfnums.find(semi->find(*v)->second)->second;
-            int u_dfnum = dfsTree->dfnums.find(semi->find(*u)->second)->second;
+        while(semi->find(dfsTree->ancestors.find(*v)->second)->second){
+            auto v1 = semi->find(*v)->second;
+            int v_dfnum = dfsTree->dfnums.find(v1)->second;
+            auto u1 = semi->find(*u)->second;
+            int u_dfnum = dfsTree->dfnums.find(u1)->second;
             if(v_dfnum < u_dfnum){
                 u = v;
             }
@@ -80,11 +82,17 @@ public:
 
 
     DominatorTree(const std::shared_ptr<CCFG>& ccfg){
-        DFSTree depth_first_spanning_tree = CreateDFSTree(ccfg);
         std::unordered_map<std::shared_ptr<basicblock>,std::vector<std::shared_ptr<basicblock>>> bucket;
         std::unordered_map<std::shared_ptr<basicblock>, std::shared_ptr<basicblock>> semi;
         std::unordered_map<std::shared_ptr<basicblock>, std::shared_ptr<basicblock>> idom;
         std::unordered_map<std::shared_ptr<basicblock>, std::shared_ptr<basicblock>> samedom;
+        DFSTree depth_first_spanning_tree = CreateDFSTree(ccfg);
+        for(auto n : depth_first_spanning_tree.nodes){
+            bucket.insert({n.second, std::vector<std::shared_ptr<basicblock>>()});
+            semi.insert({n.second, nullptr});
+            idom.insert({n.second, nullptr});
+            samedom.insert({n.second, nullptr});
+        }
         for( int i = depth_first_spanning_tree.nodes.size()-1; i > 0;i-- ){
             std::shared_ptr<basicblock> n = depth_first_spanning_tree.nodes.find(i)->second;
             std::shared_ptr<basicblock> p = depth_first_spanning_tree.parents.find(n)->second;
@@ -95,21 +103,18 @@ public:
                 if(depth_first_spanning_tree.dfnums.find(v)->second <= depth_first_spanning_tree.dfnums.find(n)->second){
                     _s = &v;
                 } else {
-                    _s = AncestorWithLowerDFNum(&n, &depth_first_spanning_tree, depth_first_spanning_tree.dfnums.find(*s)->second);
+                    _s = &(semi.find(*AncestorWithLowestSemi(&n, &depth_first_spanning_tree, &semi))->second);
                 }
                 if(depth_first_spanning_tree.dfnums.find(*_s)->second <= depth_first_spanning_tree.dfnums.find(*s)->second){
                     s = _s;
                 }
             }
-            semi.insert({n, *s});
-            auto res = bucket.insert({*s, std::vector<std::shared_ptr<basicblock>>{n}});
-            if(!(res.second)){
-                res.first->second.emplace_back(n);
-            }
+            semi.find(n)->second = *s;
+            bucket.find(*s)->second.emplace_back(n);
             Link(n, p, &depth_first_spanning_tree);
 
-            for(const auto& v : bucket.find(p)->second){
-                auto y = AncestorWithLowestSemi(&p, &depth_first_spanning_tree, &semi);
+            for(std::shared_ptr<basicblock> v : bucket.find(p)->second){
+                auto y = AncestorWithLowestSemi(&v, &depth_first_spanning_tree, &semi);
                 if(semi.find(*y)->second == semi.find(v)->second){
                     idom.insert({v, p});
                 } else {
@@ -120,7 +125,7 @@ public:
         }
         for(int i = 1; i < depth_first_spanning_tree.nodes.size(); i++){
             std::shared_ptr<basicblock> n = depth_first_spanning_tree.nodes.find(i)->second;
-            if(samedom.find(n) != samedom.end()){
+            if(samedom.find(n)->second){
                 idom.find(n)->second = idom.find(samedom.find(n)->second)->second;
             }
         }
